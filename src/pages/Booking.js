7033447e0f3ac06a463e07de2422e7658b237d56
cyberopssammurai <samuraijack@cyberopstrainings.com>
2025-03-12 05:40:57 +0530

    Create Booking.js

---
+++ src/pages/Booking.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Booking() {
+  return (
+    <div className="page">
+      <h2>Book a Consultation</h2>
+      <p>Schedule a session with our cybersecurity experts.</p>
+    </div>
+  );
+}
+
+export default Booking;
