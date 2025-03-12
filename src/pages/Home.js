6f9e5d8387d96a5ab8d6191a99c36cf259c63e89
cyberopssammurai <samuraijack@cyberopstrainings.com>
2025-03-12 05:40:22 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="page">
+      <h2>Welcome to CyberOps</h2>
+      <p>Your trusted partner in cybersecurity training and services.</p>
+    </div>
+  );
+}
+
+export default Home;
