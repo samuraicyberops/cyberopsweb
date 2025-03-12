ea92daa984063905a79ea54291c14e8db02293a0
cyberopssammurai <samuraijack@cyberopstrainings.com>
2025-03-12 05:41:26 +0530

    Create Services.js

---
+++ src/pages/Services.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div className="page">
+      <h2>Our Services</h2>
+      <p>We offer cybersecurity training, penetration testing, and more.</p>
+    </div>
+  );
+}
+
+export default Services;
