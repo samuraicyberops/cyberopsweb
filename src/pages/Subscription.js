d3be1c03a5f70d8580fcb4ab4c9ca35952379581
cyberopssammurai <samuraijack@cyberopstrainings.com>
2025-03-12 05:41:54 +0530

    Create Subscription.js

---
+++ src/pages/Subscription.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Subscription() {
+  return (
+    <div className="page">
+      <h2>Subscribe to Our Courses</h2>
+      <p>Get access to premium cybersecurity content.</p>
+    </div>
+  );
+}
+
+export default Subscription;
